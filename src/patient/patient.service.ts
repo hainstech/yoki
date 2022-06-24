06d2e9c498de942628572022c97ca4d263c6005e
Dominic Hains <contact.dominichains@gmail.com>
2022-06-24 13:21:27 -0400

    feat(\): cleanup and swagger

--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,9 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { FilledQuestionnaire } from 'src/types';
 import { User, UserDocument } from 'src/user/user.schema';
-import { UserService } from 'src/user/user.service';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { UpdatePatientDto } from './dto/update-patient.dto';
 import { Patient, PatientDocument } from './patient.schema';
@@ -12,8 +10,7 @@ import { Patient, PatientDocument } from './patient.schema';
 export class PatientService {
   constructor(
     @InjectModel(Patient.name) private patientModel: Model<PatientDocument>,
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-    private readonly userService: UserService
+    @InjectModel(User.name) private userModel: Model<UserDocument>
   ) {}
 
   async create(createPatientDto: CreatePatientDto) {
@@ -44,14 +41,5 @@ export class PatientService {
 
   remove(id: string) {
     return this.patientModel.deleteOne({ _id: id });
-  }
-
-  async pushFilledQuestionnaire(
-    id: string,
-    filledQuestionnaire: FilledQuestionnaire
-  ) {
-    const carer = await this.patientModel.findOne({ _id: id });
-    carer.filledQuestionnaires.push(filledQuestionnaire);
-    return carer.save();
   }
 }
